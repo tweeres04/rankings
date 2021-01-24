1ba57f94a1d018c9a9111edc12e66aa72f54bff7
Tyler Weeres <tweeres04@gmail.com>
2021-01-24 15:02:38 -0800

    getPlayers script to store all the players and their keys

---
+++ src/getPlayers.js
@@ -0,0 +1,76 @@
+import dotenv from 'dotenv'
+import axios from 'axios'
+import { parseStringPromise } from 'xml2js'
+import { writeFile } from 'fs/promises'
+
+dotenv.config()
+
+import { getAccessToken, refreshTheToken } from './auth.js'
+
+function playerFactory(playerData) {
+	return {
+		key: playerData.player_key,
+		name: playerData.name.full,
+		position: playerData.display_position,
+		team: playerData.editorial_team_abbr,
+	}
+}
+
+async function getPlayers(page = 0, pageSize = 25) {
+	console.log(`Fetching page ${page}`)
+	const { access_token } = await getAccessToken()
+	try {
+		const { data } = await axios.get(
+			`https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;is_available=1/leagues/players;count=100;start=${
+				page * pageSize
+			}`,
+			{
+				headers: {
+					Authorization: `Bearer ${access_token}`,
+				},
+			}
+		)
+
+		const response = await parseStringPromise(data, {
+			explicitArray: false,
+			emptyTag: null,
+		})
+
+		let players =
+			response?.fantasy_content?.users?.user?.games?.game?.leagues?.league
+				?.players?.player
+
+		players = players.map(playerFactory)
+
+		return players.length < 25
+			? players
+			: [...players, ...(await getPlayers(page + 1))]
+	} catch (err) {
+		if (err.response.status === 401) {
+			await refreshTheToken()
+			return getPlayers()
+		}
+		throw err
+	}
+}
+
+async function main() {
+	try {
+		console.log('Fetching players')
+		const results = await getPlayers()
+
+		await writeFile('data/players.json', JSON.stringify(results, null, 2))
+
+		console.table(results)
+	} catch (err) {
+		let error
+		try {
+			error = await parseStringPromise(err.response.data)
+		} catch (_) {
+			error = err
+		}
+		console.error(error)
+	}
+}
+
+main()
