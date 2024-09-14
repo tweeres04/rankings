6c31717c8b44943cc994c98fc20651a7e5ff07cc
Tyler Weeres <tweeres04@gmail.com>
2024-09-14 07:16:55 -0700

    Privacy policy

--- drafter/src/Drafter.js
+++ drafter/src/Drafter.js
@@ -71,7 +71,14 @@ export default function Drafter() {
 				className="bg-light text-center"
 				style={{ padding: '10rem 5rem' }}
 			>
-				By <a href="https://tweeres.ca">Tyler Weeres</a>
+				<div>
+					By <a href="https://tweeres.ca">Tyler Weeres</a>
+				</div>
+				<div>
+					<a href="/privacy-policy" style={{ fontSize: '0.75rem' }}>
+						Privacy Policy
+					</a>
+				</div>
 			</footer>
 			<GoogleSheetIdModal />
 		</>
--- drafter/src/index.js
+++ drafter/src/index.js
@@ -6,7 +6,26 @@ import reportWebVitals from './reportWebVitals'
 const container = document.getElementById('root')
 const root = createRoot(container)
 
-root.render(<Drafter />)
+if (window.location.pathname === '/privacy-policy') {
+	root.render(
+		<div className="container">
+			<h1>Drafter Privacy Policy</h1>
+			<ul>
+				<li>
+					I don't have access to any of your data — it's only stored
+					locally in your browser for application use.
+				</li>
+				<li>
+					I will likely collect anonymous product usage data in the
+					future in order to improve the product
+				</li>
+				<li>I won't sell or give away any personal information</li>
+			</ul>
+		</div>
+	)
+} else {
+	root.render(<Drafter />)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
